4eb03ef19e5e7d1c9ca868ed47fa86bacf6d190f
ComeDownToUs <blues4ceausescu@gmail.com>
2017-04-12 01:47:30 +0100

    initial srt read process and tests

---
+++ subtitleIO.py
@@ -0,0 +1,90 @@
+import re
+import datetime
+
+#class
+  #dialogue array (line by line)
+  #milliseconds {start, end}
+  #def inframes(frames)
+  #def inseconds
+
+class SubLine:
+  def __init__(self, subs, start, end):
+    self.dialogue = []
+    for x in subs:
+      self.dialogue.append(x)
+    self.time = {
+      'start': start,
+      'end': end
+    }
+  def hms_to_frames(self, frames):
+    return frames
+  def hms_to_seconds(self):
+    return 0
+
+#just storing the file reading process within a function
+def read_subtitles(sub_file):
+  f = open(sub_file, 'r')
+  text = f.read()
+  f.close()
+  return text
+
+def write_subtitles(sub_file, sub_text):
+  f = open(sub_file, 'w')
+  f.write(subtext)
+  f.close()
+  return subtext
+
+def subs_to_time(subs_time):
+  hours = int(subs_time[0])
+  minutes = int(subs_time[1])
+  seconds = int(subs_time[2])
+  microsec = int(subs_time[3])*1000#srt is in thousandths of a second
+  return datetime.time(hours, minutes, seconds, microsec)
+
+def process_srt(sub_text):
+  #a blank line represents a next section
+  basic_split = sub_text.strip().split('\n\n')
+  formatted_lines = []
+  for subtitle in basic_split:
+    subtitle = subtitle.split('\n')
+    times_str = subtitle[1].split('-->')
+    time_formatted = []
+    for time_str in times_str:
+      time_str = time_str.strip().replace(',', ':').split(':')
+      time_formatted.append(subs_to_time(time_str))
+    dialogue_lines = []
+    for dialogue_index in range(2, len(subtitle)):
+      dialogue_lines.append(subtitle[dialogue_index])
+    formatted_lines.append(SubLine(dialogue_lines, time_formatted[0], time_formatted[1]))
+  return formatted_lines
+
+#process ssa
+# formatting from here https://matroska.org/technical/specs/subtitles/ssa.html
+def process_ssa(sub_text):
+  #a blank line represents a next section
+  segment_split = re.split('\n\[(.*)\]\n', sub_text)
+  #should split into 3-5 segments
+  #segment three contains the events, so it's the one we'll be working with
+  formatted_lines = []
+  dialogue_lines=segment_split[2].split('\n')
+  dialogue_format=dialogue_lines.pop(0)
+  #get indexes of relevant format pieces
+  for subtitle in dialogue_lines:
+    #resolve indexes
+    fragments = subtitle.split(',')
+  print str(formatted_lines)
+  return formatted_lines
+
+#process .sub
+  #convert frames to seconds
+  #split dialogue by '|'
+
+
+#regular converters, reformats subtitles to a string in the correct format
+#write sub
+
+#write ssa
+
+#write srt
+
+
---
+++ tests.py
@@ -0,0 +1,56 @@
+import unittest
+import subtitleIO
+import datetime
+
+srt_verifier = [subtitleIO.SubLine(['BROKEN EMBRACES'], datetime.time(0, 1, 12, 833*1000), datetime.time(0, 1, 19, 0) )]
+ssa_verifier = [subtitleIO.SubLine(['BROKEN EMBRACES'], datetime.time(0, 1, 12, 83*10000), datetime.time(0, 1, 19, 0) )]
+srt_testfile = 'test_files/SRTTest.srt'
+ssa_testfile = 'test_files/SSATest.ssa'
+
+#basic reading and writing tests
+class TestCore(unittest.TestCase):
+
+  def __init__(self, *args, **kwargs):
+    super(TestCore, self).__init__(*args, **kwargs)
+    self.srtSubsFormat = subtitleIO.process_srt(subtitleIO.read_subtitles(srt_testfile))
+    self.ssaSubsFormat = subtitleIO.process_ssa(subtitleIO.read_subtitles(ssa_testfile))
+  
+  def test_read_srt_dialogue(self):
+    self.assertEqual(self.srtSubsFormat[0].dialogue, srt_verifier[0].dialogue)
+
+  def test_read_srt_times(self):
+    self.assertEqual(self.srtSubsFormat[0].time, srt_verifier[0].time)
+  
+  def _write_srt():
+    #write as srt to file
+    return 0
+  
+  def read_ssa_dialogue(self):
+    self.assertEqual(self.ssaSubsFormat[0].dialogue, ssa_verifier[0].dialogue)
+
+  def read_ssa_times(self):
+    self.assertEqual(self.ssaSubsFormat[0].time, ssa_verifier[0].time)
+
+  def write_ssa():
+    return 0
+    #write as ssa to file, compare
+
+class TestProcessConfig(unittest.TestCase):
+  def readtes():
+    return 0
+
+class TestMerges(unittest.TestCase):
+  def __init__(self, *args, **kwargs):
+    super(TestConversion, self).__init__(*args, **kwargs)
+    self.srtSubsFormat = subtitleIO.process_srt(subtitleIO.read_subtitles(srt_testfile))
+    self.ssaSubsFormat = subtitleIO.process_ssa(subtitleIO.read_subtitles(ssa_testfile))
+  
+  def pair_lines(self):
+    return 0
+
+  def write_file(self):
+    return 0
+  
+
+if __name__ == '__main__':
+    unittest.main()
