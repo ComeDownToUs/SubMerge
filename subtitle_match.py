f51aa333c4d304f039dbae6e92c56446c8df07e6
ComeDownToUs <blues4ceausescu@gmail.com>
2017-05-06 00:17:44 +0100

    refactoring functions

--- subtitle_match.py
+++ subtitle_match.py
@@ -1,6 +1,7 @@
 import subtitleIO
 
 #this is all messy and untested plotting at the moment
+#for SSA, both sets of subtitles appearing at the same time seems pretty essential
 def match_process(sublistA, sublistB):
   counter_B = 0
   for subtitle in sublistA:
@@ -23,6 +24,7 @@ def get_time_range(root_time, variance=1000):
   return start_point, end_point
 
 #just reduces a subtitle's dialogue proportions down to one string with no linebreaks
+#only needed if space on screen is an issue
 def remove_newlines(sublist, newline_replacement = "--"):
   for i in range(len(sublist)):
     if len(sublist[i].dialogue)>1:
@@ -32,20 +34,27 @@ def remove_newlines(sublist, newline_replacement = "--"):
       sublist[i].dialogue = [merged_dialogue]
   return sublist
 
-#apply stylings to secondary subs,
+#apply stylings to a string,
 #need to test which ones need closing
 def ssa_apply_styling(sublist, style_change):
-  styling = ""
+  styling = ["", ""]
   if "bold" is in style_change:
-    styling = "{\b1}"+styling
-    #styling[1] += styling[0]+"{\b0}"
+    styling[0] = "{\\b1}"+styling[0]
+    styling[1] += styling[1]+"{\\b0}"
   if "fs12" is in style_change:
-    styling = "{\fs12}"+styling
+    styling[0] = "{\\fs12}"+styling[0]
+    styling[1] += styling[1]+"{\\r}"#will override all overrides
   if "italics" is in style_change:
-    styling = "{\fs12}"+styling
-  for i in range(len(sublist)):
-    sublist[i].dialogue[0] = styling + sublist[i].dialogue[0]
-  return sublist
+    styling[0] = "{\i1}"+styling[0]
+    styling[1] = styling[0]+"{\i0}"
+  if "underline" is in style_change:
+    styling[0] = "{\u1}"+styling[0]
+    styling[1] = styling[0]+"{\u0}"
+  if "strike" is in style_change:
+    styling[0] = "{\s1}"+styling[0]
+    styling[1] = styling[0]+"{\s0}"
+  #return styling[0]+dialogue+styling[1]
+  return styling
 
 
 
